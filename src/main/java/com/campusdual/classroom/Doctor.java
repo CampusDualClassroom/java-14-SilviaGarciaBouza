a0baa8de6ab9921d0e2f0098c76d4ec92dc94f88
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-05-23 13:33:41 +0200

    Ejercico 14 completado

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -8,4 +8,9 @@ public class Doctor extends Person {
 		super(name, surname);
 		this.specialization = specialization;
 	}
+
+	@Override
+	public void getDetails() {
+		System.out.println("Doctor: nombre: "+super.name+", apellido: "+super.surname+", especializacion: "+this.specialization);
+	}
 }
--- src/main/java/com/campusdual/classroom/PoliceOfficer.java
+++ src/main/java/com/campusdual/classroom/PoliceOfficer.java
@@ -8,4 +8,8 @@ public class PoliceOfficer extends Person {
 		super(name, surname);
 		this.squad = squad;
 	}
+	@Override
+	public void getDetails() {
+		System.out.println("Oficial de policia: nombre: "+super.name+", apellido: "+super.surname+", escuadron: "+this.squad);
+	}
 }
